e5d4c942b500de35d8d2028969ff563dd5c1e08a
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-08-19 10:55:45 -0400

    Update matlabMobileParsor.m

--- MatLab Code (Motif & MatLab Mobile)/matlabMobileParsor.m
+++ MatLab Code (Motif & MatLab Mobile)/matlabMobileParsor.m
@@ -7,48 +7,46 @@ date = accelerationData.Timestamp
 ts=seconds(date-date(1)); % timestamp in s
 f_s = 100;   % sampling rate 
 Ts= 1/f_s;
-u1 = accelerationData.X
-u2 = accelerationData.Y
-u3 = accelerationData.Z
+acc_x = accelerationData.X
+acc_y = accelerationData.Y
+acc_z = accelerationData.Z
 
 disp(ts)
 
 % figure;
 subplot(2,3,1);
-plot(ts,u1,'k');
+plot(ts,acc_x,'k');
 ylabel(' $ X(t) $', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 xlabel('$t$ (s)', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 
 subplot(2,3,2);
-plot(ts,u2,'k');
+plot(ts,acc_y,'k');
 ylabel(' $ X(t) $', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 xlabel('$t$ (s)', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 
 subplot(2,3,3);
-plot(ts,u3,'k');
+plot(ts,acc_z,'k');
 ylabel(' $ X(t) $', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 xlabel('$t$ (s)', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 % xlim([0,12]);
 % stop;
 
-
- 
 %  figure,
-[f1 fabs1]=fftplot(Ts,u1);
+[f1 fabs1]=fftplot(Ts,acc_x);
 subplot(2,3,4);
 plot(f1,fabs1,'k');
 xlim([0,15]);
 ylabel(' $ X(f) $', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 xlabel('$f$ (Hz)', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 
-[f2 fabs2]=fftplot(Ts,u2);
+[f2 fabs2]=fftplot(Ts,acc_y);
 subplot(2,3,5);
 plot(f2,fabs2,'k');
 xlim([0,15]);
 ylabel(' $ X(f) $', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 xlabel('$f$ (Hz)', 'Interpreter','latex','FontSize', 14, 'FontName', 'Times New Roman');
 
-[f3 fabs3]=fftplot(Ts,u3);
+[f3 fabs3]=fftplot(Ts,acc_z);
 subplot(2,3,6);
 plot(f3,fabs3,'k');
 xlim([0,15]);
